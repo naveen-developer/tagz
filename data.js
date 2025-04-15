39e4ad1389241e78a18b86159fc9d3dcdf2ccc7d
naveen-developer <you@example.com>
2025-04-15 22:15:34 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,122 @@
+
+let Products = [
+    {
+        "id": 1,
+        "title": "Pack of 3 Gourmet Dips",
+        "actualPrice": "375",
+        "discountPrice": "319",
+        "status": "14% OFF",
+        "image_url": "https://tagzfoods.com/cdn/shop/products/Packof1eachofRanch_Harissa_Chipotle_360x.png?v=1670324158",
+        "background_url": "https://tagzfoods.com/cdn/shop/products/Chipotle_Back_1_360x.png?v=1670324158"
+    },
+    {
+        "id": 2,
+        "title": "Sweet & Savoury Combo",
+        "actualPrice": "410",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/frontdesign_360x.png?v=1694607063",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/BBQBACKANDFRONT_360x.png?v=1694607647"
+    },
+    {
+        "id": 3,
+        "title": "All in one combo",
+        "actualPrice": "469",
+        "discountPrice": "",
+        "status": "SOLD OUT",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Untitleddesign_12_180x.png?v=1694778188",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/BBQFRONT_BACK_59d73301-e8d4-421f-a374-f329769bb057.jpg?v=1694845821"
+    },
+    {
+        "id": 4,
+        "title": "Creamy Fiesta",
+        "actualPrice": "539",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Untitleddesign_5_360x.png?v=1694782638",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/CREAMING_1_c9ed11bf-71bb-40d0-a93c-0df7c12e0f2e_360x.png?v=1694782637"
+    },
+    {
+        "id": 5,
+        "title": "Cocktail Special Combo",
+        "actualPrice": "539",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Untitleddesign_14_180x.png?v=1694845826",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/BBQFRONT_BACK_2700x.jpg"
+    },
+    {
+        "id": 6,
+        "title": "Party Snack",
+        "actualPrice": "330",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Untitleddesign_2_1_180x.png?v=1694849536",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/TOMYUMFRONT_BACK_2ab4f8e4-af4c-483c-9e98-fb8ebd8de569.jpg"
+    },
+    {
+        "id": 7,
+        "title": "FLAVOR VLAST",
+        "actualPrice": "180",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/CopyofPackof3_Masala_Cream_BBQ_-Rs.60Each_360x.png?v=1694852237",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/BBQ52G-01_360x.png?v=1694852234"
+    },
+    {
+        "id": 8,
+        "title": "Best sellers - 5 in 1",
+        "actualPrice": "300",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Cookies2ftprintcopy.pdf_1_360x.png?v=1694852640",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/BBQ52G-01_360x.png?v=1694852234"
+    },
+    {
+        "id": 9,
+        "title": "Chip & Dip Delight Combo",
+        "actualPrice": "399",
+        "discountPrice": "",
+        "status": "SOLD OUT",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Cookies2ftprintcopy.pdf_1_360x.png?v=1694852640",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/BBQ52G-01_360x.png?v=1694852234"
+    },
+    {
+        "id": 10,
+        "title": "Party Slayer",
+        "actualPrice": "439",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Untitleddesign_13_1_180x.png?v=1694854610",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/CREAMONIONFRONTANDBACK52G-01_jpg_3642f730-3c43-41ef-b651-8a0ea7612e29_360x.png?v=1694854608"
+    },
+    {
+        "id": 11,
+        "title": "Snack Attack Combo",
+        "actualPrice": "220",
+        "discountPrice": "",
+        "status": "SOLD OUT",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/packof3_cookie_1_360x.png?v=1694865716",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/CREAMONIONFRONTANDBACK52G-01_jpg_3642f730-3c43-41ef-b651-8a0ea7612e29_360x.png?v=1694854608"
+    },
+    {
+        "id": 12,
+        "title": "Spice Fiesta",
+        "actualPrice": "539",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Untitleddesign_3_1_180x.png?v=1694844697",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/MASALAFRONT_BACK_6bbdaf6c-d52e-4329-a1f0-87787ab00f6e.jpg?v=1694844696"
+    },
+    {
+        "id": 13,
+        "title": "High on Life - Hemp CookieZ",
+        "actualPrice": "160",
+        "discountPrice": "",
+        "status": "",
+        "image_url": "https://tagzfoods.com/cdn/shop/files/Packof4cookiez_360x.png?v=1694850205",
+        "background_url": "https://tagzfoods.com/cdn/shop/files/2_98420276-b2a1-4b98-85ac-d8b827865773_360x.png?v=1694850204"
+    }
+];
+
+
